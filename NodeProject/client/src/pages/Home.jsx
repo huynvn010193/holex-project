cea097ef419b264b7874b9600e47f078961504a4
huynvn <Huy.Nguyen@navigosgroup.com>
2023-07-23 21:49:12 +0700

    Create UI list Folder

--- NodeProject/client/src/pages/Home.jsx
+++ NodeProject/client/src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
+import UserMenu from "../components/UserMenu";
+import FolderList from "../components/FolderList";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Typography variant='h4' sx={{ mb: "20px" }}>
+        Note App
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "right", mb: "10px" }}>
+        <UserMenu />
+      </Box>
+      <Grid
+        container
+        sx={{ height: "50vh", boxShadow: "0 0 15px 0 rgb(193 193 193 / 60%)" }}
+      >
+        <Grid item xs={3} sx={{ height: "100%" }}>
+          <FolderList
+            folders={[
+              { id: "1", name: "Plan for Tet holiday" },
+              { id: "2", name: "New folder" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={9} sx={{ height: "100%" }}>
+          <p>Note List</p>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
