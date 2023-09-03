173c0f826a2c04d12efdbc50ded4c2a5110c3393
huynvn010193 <hpn010193@gmail.com>
2023-09-03 21:29:15 +0700

    Update code FE error link

--- NodeProject/client/src/utils/constants.js
+++ NodeProject/client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = "https://huynvn-note-app.onrender.com";
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  "wss://https://huynvn-note-app.onrender.com/graphql";
+  "wss://huynvn-note-app.onrender.com/graphql";
