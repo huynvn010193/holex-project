0ce78adce9d1bc4327e12a0239d8d9825a1f1824
huynvn010193 <hpn010193@gmail.com>
2023-09-03 21:22:31 +0700

    Update code

--- NodeProject/client/src/utils/constants.js
+++ NodeProject/client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = "https://huynvn-note-app.onrender.com";
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  "wss://https://huynvn-note-app.onrender.com/grapql";
+  "wss://https://huynvn-note-app.onrender.com/graphql";
