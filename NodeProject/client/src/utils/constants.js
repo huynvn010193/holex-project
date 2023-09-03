6f69e70220c760bbe72eba3d799984092cea7d32
huynvn010193 <hpn010193@gmail.com>
2023-09-03 21:17:41 +0700

    Update code FE link 2

--- NodeProject/client/src/utils/constants.js
+++ NodeProject/client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = "https://huynvn-note-app.onrender.com";
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  "ws://https://huynvn-note-app.onrender.com";
+  "wss://https://huynvn-note-app.onrender.com/grapql";
